9f55bb9800aeb9c270a2838ab1b61c896e4e3fab
LinOS <linusstraehle@web.de>
2025-12-17 20:49:57 +0100

    Enhance sortFilesByModificationDate functionality
    Updated the function to sort file paths by either modification date or filename based on parameters.

--- src/func/sortFilesByModificationDate.m
+++ src/func/sortFilesByModificationDate.m
@@ -1,40 +1,50 @@
-function sortedFilePaths = sortFilesByModificationDate(filePaths,Parameters)
-    % sortFilesByModificationDate Sorts a list of file paths by last modification date.
-    % 
-    % INPUT:
-    %   filePaths - A cell array or string array of file paths (e.g., {"path/to/file1", "path/to/file2"}).
-    %
-    % OUTPUT:
-    %   sortedFilePaths - A cell array or string array of file paths sorted by modification date (oldest first).
+function sortedFilePaths = sortFilesByModificationDate(filePaths, Parameters)
+%SORTFILESBYMODIFICATIONDATE  Sort file paths by modification date or name.
+%
+%   sortedFilePaths = sortFilesByModificationDate(filePaths, Parameters)
+%
+%   Sorts a list of file paths either by their last modification date
+%   (oldest first) or alphabetically by filename, depending on the
+%   configuration in Parameters.General.SampleOrder.
+%
+%   Inputs:
+%     filePaths  - Cell array or string array of file paths
+%     Parameters - Project parameters struct containing:
+%                  Parameters.General.SampleOrder
+%                  0 = sort by modification date
+%                  1 = sort by filename
+%
+%   Output:
+%     sortedFilePaths - Sorted list of file paths
+%
 
-    % Initialize an array to hold the modification dates
     numFiles = length(filePaths);
-    modificationDates = NaT(1, numFiles); % NaT: Not-a-Time for datetime array
-    if Parameters.General.SampleOrder == 0 % Sort by Modification Date
-   
+    modificationDates = NaT(1, numFiles); % Preallocate datetime array
 
-    % Loop through each file path to retrieve the modification date
-    for i = 1:numFiles
-        fileInfo = dir(filePaths{i});  % Get file information 
-        %try 
-        fileDateNum = fileInfo.datenum;
-        modificationDates(i) = datetime(fileDateNum, 'ConvertFrom', 'datenum', 'InputFormat', 'dd-MMM-yyyy HH:mm:ss', 'Locale', 'en_US');
-            
-            %datetime(fileInfo.date, 'InputFormat', 'dd-MMM-yyyy HH:mm:ss', 'Locale', 'en_US');
-        %catch
-            %modificationDates(i) = datetime(fileInfo.datenum, 'InputFormat', 'dd-MMM-yyyy HH:mm:ss', 'Locale', 'de_DE');
-        %end
-    end
-    
-    % Sort the file paths by modification dates
-    [~, sortIdx] = sort(modificationDates);  % Get indices of sorted dates
-    sortedFilePaths = filePaths(sortIdx);  % Sort the file paths accordingly
-    elseif Parameters.General.SampleOrder == 1 % Sort by Name
-        sortedFilePaths = sort(filePaths,2,"ascend");
-    % Display sorted file paths (optional)
-    % disp('Sorted file paths (oldest modification date first):');
-    % disp(sortedFilePaths);
+    if Parameters.General.SampleOrder == 0
+        %% Sort by modification date
+
+        for i = 1:numFiles
+            fileInfo = dir(filePaths{i}); % Get file information
+            fileDateNum = fileInfo.datenum;
+
+            modificationDates(i) = datetime( ...
+                fileDateNum, ...
+                'ConvertFrom', 'datenum', ...
+                'InputFormat', 'dd-MMM-yyyy HH:mm:ss', ...
+                'Locale', 'en_US' ...
+            );
+        end
+
+        % Sort by date (oldest first)
+        [~, sortIdx] = sort(modificationDates);
+        sortedFilePaths = filePaths(sortIdx);
+
+    elseif Parameters.General.SampleOrder == 1
+        %% Sort by filename
+        sortedFilePaths = sort(filePaths, 2, "ascend");
     end
 end
+
 
 
