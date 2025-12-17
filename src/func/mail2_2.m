5b81737d8804bae67e8f2479029d1d1ca102759f
LinOS <linusstraehle@web.de>
2025-12-17 20:35:09 +0100

    Refactor mail2_2 to send HTML emails via PowerShell
    Updated the mail2_2 function to send HTML emails using PowerShell's Send-MailMessage. Improved documentation and streamlined the message conversion process.

--- src/func/mail2_2.m
+++ src/func/mail2_2.m
@@ -1,30 +1,40 @@
-function mail2_2( user,...
-                  subject,...
-                  message,...
-                  attachment)% exclist
-% by Tobias Bader 2022-04-06
-% new version which uses powershell and dummy.d@lw-online to send messages
-% Adapted by Michael Mohr 2025-06-16
-M1 = strjoin(message,'<br />'); % changed from \n to HTML version (powershell)
-%M2 = strjoin(message,' | ');
+function mail2_2(user, subject, message, attachment)
+%MAIL2_2  Send emails via PowerShell using Send-MailMessage.
+%
+%   mail2_2(user, subject, message, attachment)
+%   Sends an email to one or multiple recipients using PowerShell's
+%   Send-MailMessage command and a predefined SMTP server.
+%
+%   Inputs:
+%     user       - Recipient email address or array of addresses
+%     subject    - Email subject (string or char)
+%     message    - Cell array of message lines (will be converted to HTML)
+%     attachment - Full path to file attachment
+%
+%   Notes:
+%     - The email body is sent as HTML.
+%     - The sender address is fixed to dummy.d@lw-online.de.
+%     - Requires PowerShell and access to the SMTP server.
+%
 
-for i = 1:size(user,2)
-   
-    argument = strcat({'powershell -command "Send-MailMessage -BodyAsHtml -From '}, {'dummy.d@lw-online.de'},...
-                      {' -Subject '},{''''},strcat(string(datetime(now,'ConvertFrom','datenum')),{' '},subject),{''''},...
-                      {' -To '},{''''},user(i),{''''},...
-                      {' -Body '},{''''}, string(M1),{''''},...
-                      {' -SmtpServer Mail.lw-online.de'},...
-                      {' -Attachments "'}, attachment, {'"'} ,'"');
-    system(char(join(argument)));     
-         
-end
-% documentation
-% [~, existinglist]  = xlsread( exclist,'mails_cal' );
-% xlswrite( exclist,...
-%           cellstr(strcat( string(datetime(now,'ConvertFrom','datenum')),...
-%           '_', strjoin(user,'_'), '_', subject, '_', M2)),...
-%           'mails_cal',strcat('A',num2str(size(existinglist,1)+1)) )
-end
+    % Convert message cell array to HTML body
+    M1 = strjoin(message, '<br />');
 
+    % Loop over all recipients
+    for i = 1:size(user, 2)
+
+        argument = strcat( ...
+            {'powershell -command "Send-MailMessage -BodyAsHtml -From '}, ...
+            {'dummy.d@lw-online.de'}, ...
+            {' -Subject '}, {''''}, ...
+            strcat(string(datetime(now,'ConvertFrom','datenum')), {' '}, subject), {''''}, ...
+            {' -To '}, {''''}, user(i), {''''}, ...
+            {' -Body '}, {''''}, string(M1), {''''}, ...
+            {' -SmtpServer Mail.lw-online.de'}, ...
+            {' -Attachments "'}, attachment, {'"'} , '"' ...
+        );
+
+        system(char(join(argument)));
+    end
+end
 
