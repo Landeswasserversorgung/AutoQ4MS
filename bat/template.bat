09a0cdb9e5f62380577fa1281d8002a08ccc6d05
LinOS <linusstraehle@web.de>
2025-12-17 21:25:20 +0100

    Update comments in template.bat to English

--- bat/template.bat
+++ bat/template.bat
@@ -4,21 +4,21 @@ cd /d "%PROJECT_PATH%"
 
 echo [AutoQ4MS] Processing is executed...
 
-REM Methode extrahieren (Dateiname ohne Pfad und Endung)
+REM Extract method (file name without path and extension)
 for %%f in ("%METHOD_PATH%") do set "METHOD_NAME=%%~nf"
 
-REM Zeitstempel erzeugen (Format: YYYYMMDD-HHMM)
+REM create time stamp (Format: YYYYMMDD-HHMM)
 for /f %%a in ('powershell -command "Get-Date -Format yyyyMMdd-HHmm"') do set "TS=%%a"
 
-REM Log-Dateipfad zusammensetzen
+REM Compose log file path
 set LOG_PATH=%LOGS_PATH%\logs\%METHOD_NAME%_%TS%.log
 
-REM Vorherige Log-Datei löschen (optional)
+REM Delete previous log file (optional)
 if exist "%LOG_PATH%" del "%LOG_PATH%"
 
 "%MATLAB_EXE%" -batch "try,  setup(); processing('%METHOD_PATH%'); disp('[AutoQ4MS] processing finished successfully.'); exit(0); catch e, fid = fopen('%LOG_PATH%', 'w'); msg = getReport(e, 'extended', 'hyperlinks', 'off'); fprintf(fid, 'FAIL\n%%s\n', msg); fclose(fid); exit(1); end"
 
-REM Fehler prüfen anhand Log
+REM Check errors using log
 findstr /C:"FAIL" "%LOG_PATH%" >nul
 IF %ERRORLEVEL% EQU 0 (
     echo Fehler beim Ausführen von MATLAB! Siehe log:
